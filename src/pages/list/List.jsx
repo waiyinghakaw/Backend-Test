93849d74ca71febe9523683f856a7571676ada0d
Miguel Francis M. Estañol <miguel_estanol@dlsu.edu.ph>
2023-07-23 14:11:56 +0800

    added search result webpage

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -3,12 +3,14 @@ import Promobar from '../../components/promobar/Promobar'
 import Navbar from '../../components/navbar/Navbar'
 import Searchres from '../../components/searchres/Searchres'
 import Footer from '../../components/footer/Footer'
+import Desktopnav from '../../components/desktopnav/Desktopnav'
 
 const List = () => {
   return (
     <div className='search-results'>
       <Promobar />
       <Navbar />
+      <Desktopnav />
       <Searchres />
       <Footer />
     </div>
