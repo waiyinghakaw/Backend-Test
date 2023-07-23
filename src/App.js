29157844f2a9b8353b7e1ec5db45d635ec9beda9
Miguel Francis M. Estañol <miguel_estanol@dlsu.edu.ph>
2023-07-23 08:20:13 +0800

    Added Pages Structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { 
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Home from "./pages/home/Home"
+import List from "./pages/list/List"
+import Service from "./pages/service/Service"
+
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/listings" element={<List/>}/>
+        <Route path="/listings/:id" element={<Service/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
