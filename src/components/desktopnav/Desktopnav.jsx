f14727fd7288c75928b2f3c3b14e8cef82dc16c9
Miguel Francis M. Estañol <miguel_estanol@dlsu.edu.ph>
2023-07-23 11:32:38 +0800

    Added desktop navigation menu

--- src/components/desktopnav/Desktopnav.jsx
+++ src/components/desktopnav/Desktopnav.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 
 const Desktopnav = () => {
-  return (
-    <div>Desktopnav</div>
-  )
+    return (
+        <div className="desktopnav">
+            <nav className="desktop-navigation-menu">
+                <div className="container">
+                    <ul className="desktop-menu-category-list">
+                        <li className="menu-category">
+                            <a href="homepage.html" class="menu-title">Home</a>
+                        </li>
+
+                        <li className="menu-category">
+                            <a href="listings.html" class="menu-title">Service Listings</a>
+                        </li>
+
+                        <li className="menu-category">
+                            <a href="create_listing.html" class="menu-title"
+                            >Create a Listing</a
+                            >
+                        </li>
+
+                        <li className="menu-category">
+                            <a href="delete-listings.html" class="menu-title"
+                            >View My Listings</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default Desktopnav
